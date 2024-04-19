f0020017afae48a2e3d4bcb2acf5f0f5ae2be99f
Rmojarro1 <48000819+Rmojarro1@users.noreply.github.com>
2024-04-19 12:31:28 -0700

    finish

--- src/Scenes/Monster.js
+++ src/Scenes/Monster.js
@@ -6,6 +6,35 @@ class Monster extends Phaser.Scene {
         //Create constants for the monster location
         this.bodyX = 300;
         this.bodyY = 350;
+
+        this.rightArmX = this.bodyX + 100;
+        //this.rightArmY = this.bodyY + 20;
+        this.leftArmX = this.bodyX - 90;
+        this.leftArmY = this.bodyY + 40;
+
+        this.rightLegX = this.bodyX + 60;
+        this.rightLegY = this.bodyY + 110;
+        this.leftLegX = this.bodyX - 50;
+        this.leftLegY = this.bodyY + 110;
+
+        this.rightStalkX = this.bodyX + 40; 
+        this.rightStalkY = this.bodyY - 100; 
+        this.leftStalkX = this.bodyX - 30; 
+        this.leftStalkY = this.bodyY -100; 
+
+        this.rightEyeX = this.bodyX + 40; 
+        this.rightEyeY = this.bodyY - 113; 
+        this.leftEyeX = this.bodyX - 40; 
+        this.leftEyeY = this.bodyY - 120; 
+
+        this.centerEyeX = this.bodyX;
+        this.centerEyeY = this.bodyY - 30;  
+
+        this.smileX = this.bodyX; 
+        this.smileY = this.bodyY + 20; 
+        this.fangX = this.bodyX; 
+        this.fangY = this.bodyY + 20; 
+        
         
     }
 
@@ -31,15 +60,63 @@ class Monster extends Phaser.Scene {
         //
         // look in spritesheet_default.xml for the individual sprite names
         // You can also download the asset pack and look in the PNG/default folder.
-        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_greenD.png");
+        my.sprite.body = this.add.sprite(this.bodyX, this.bodyY, "monsterParts", "body_darkD.png");
+        my.sprite.rightArm = this.add.sprite(this.rightArmX, this.rightArmY, "monsterParts", "arm_yellowA.png"); 
+        my.sprite.leftArm = this.add.sprite(this.leftArmX, this.leftArmY, "monsterParts", "arm_yellowE.png"); 
+        my.sprite.leftArm.flipX = true; 
 
-        
+        my.sprite.rightLeg = this.add.sprite(this.rightLegX, this.rightLegY, "monsterParts", "leg_yellowC.png"); 
+        my.sprite.leftLeg = this.add.sprite(this.leftLegX, this.leftLegY, "monsterParts", "leg_yellowC.png"); 
+        my.sprite.leftLeg.flipX = true; 
+
+        my.sprite.rightStalk = this.add.sprite(this.rightStalkX, this.rightStalkY, "monsterParts", "detail_yellow_eye.png");
+        my.sprite.leftStalk = this.add.sprite(this.leftStalkX, this.leftStalkY, "monsterParts", "detail_yellow_antenna_large.png"); 
+        my.sprite.leftStalk.flipX = true; 
+
+        my.sprite.rightEye = this.add.sprite(this.rightEyeX, this.rightEyeY, "monsterParts", "eye_red.png");
+        my.sprite.leftEye = this.add.sprite(this.leftEyeX, this.leftEyeY, "monsterParts", "eye_yellow.png"); 
+        my.sprite.centerEyeClosed = this.add.sprite(this.centerEyeX, this.centerEyeY, "monsterParts", "mouth_closed_sad.png");
+        my.sprite.centerEyeOpen = this.add.sprite(this.centerEyeX, this.centerEyeY, "monsterParts", "eye_cute_light.png");
+        my.sprite.centerEyeOpen.visible = false; 
+
+        my.sprite.smile = this.add.sprite(this.smileX, this.smileY, "monsterParts", "mouth_closed_fangs.png"); 
+        my.sprite.smile.flipY = true; 
+        my.sprite.fang = this.add.sprite(this.smileX, this.smileY, "monsterParts", "mouthI.png"); 
+        my.sprite.fang.visible = false; 
+
+        this.input.keyboard.on('keydown-S', (event) => {
+            my.sprite.fang.visible = false; 
+            my.sprite.smile.visible = true; 
+        });
+
+        this.input.keyboard.on('keydown-F', (event) => {
+            my.sprite.fang.visible = true; 
+            my.sprite.smile.visible = false; 
+        }); 
+
+        this.input.keyboard.on('keydown-D', (event) => {
+            for (const spriteName in this.my.sprite) {
+                const thisSprite = this.my.sprite[spriteName];
+                thisSprite.x += 10;
+              }
+        })
+
+        this.input.keyboard.on('keydown-A', (event) => {
+            for(const spriteName in this.my.sprite){
+                const thisSprite = this.my.sprite[spriteName]; 
+                thisSprite.x -= 10; 
+            }
+        })
+    
+    
+    
     }
+
+       
+            
 
     update() {
         let my = this.my;    // create an alias to this.my for readability
-
-       
     }
 
 }
